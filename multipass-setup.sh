8b62e5f40ee876b369c2978672ea3fb2af8dcb25
Palassis <40472433+MxNxPx@users.noreply.github.com>
2020-01-07 19:56:40 -0500

    Update multipass-setup.sh

--- multipass-setup.sh
+++ multipass-setup.sh
@@ -26,23 +26,29 @@ sudo apt-get install -y kubectl
 wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube-linux-amd64
 sudo mv minikube-linux-amd64 /usr/local/bin/minikube
-echo "done"
 EOF
 
 ## launch multipass
 multipass launch ubuntu --name $NAME --cpus $CPU --mem $MEM --disk $DISK
 sleep 10
-multipass list | egrep "^ubuntu-multipass" | grep Running
+multipass list | egrep "^ubuntu-multipass.*Running.*([0-9]{1,3}[\.]){3}[0-9]{1,3}"
 if [ $? -ne 0 ]; then 
+   echo "[!] multipass instance failed to create, run command below and try again:"
+   echo "    #  multipass delete ubuntu-multipass && multipass purge"
    exit 1
 fi
 
 ## loop thru commands
 OLDIFS=$IFS
 IFS=$'\n'
+echo "[*] `date` -- RUNNING THRU INSTALLS ..."
 for line in $(cat multipass-commands.txt); do
-  echo $line
+  echo "[*] $line"
   multipass exec $NAME -- bash -c ''"$line"''
 done
+echo "[*] `date` -- DONE WITH INSTALLS ..."
 IFS=$OLDIFS
 rm multipass-commands.txt
+
+## copy files prefaced with "multipass" into the multipass instance
+multipass copy-files multipass* $NAME:
